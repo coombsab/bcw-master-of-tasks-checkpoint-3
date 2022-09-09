f608d7e710c7da72e3e802dfee39f46508dd93cf
Abraham Coombs <coombsab@gmail.com>
2022-09-09 16:52:03 -0600

    Initial finish of all minimum requirements

--- app/Models/List.js
+++ app/Models/List.js
@@ -14,11 +14,13 @@ export class List {
   get Template() {
     return /*html*/`
       <div class="col-12 col-md-5 col-lg-4 col-xl-3 mb-2 mb-md-4">
-        <div class="card d-flex flex-column p-3 elevation-1">
-          <i class="mdi mdi-close-box align-self-end selectable" onclick="app.listsController.removeList('${this.id}')"></i>
-          <div class="card-header text-center">
-            <h4>${this.name.toUpperCase()}</h4>
-            <p>${this.UnCheckedItems.length}/${this.Items.length}</p>
+        <div class="card d-flex flex-column rounded elevation-1">
+          <div class="rounded d-flex flex-column" style="background-color: ${this.color}">
+            <i class="mdi mdi-close-box align-self-end selectable text-bubble" onclick="app.listsController.removeList('${this.id}')"></i>
+            <div class="card-header d-flex flex-column justify-content-center align-items-center">
+                <h4 class="text-bubble">${this.name.toUpperCase()}</h4>
+                <p class="text-bubble">${this.UnCheckedItems.length}/${this.Items.length}</p>
+            </div>
           </div>
           <div class="card-body">
             <div>
@@ -28,7 +30,7 @@ export class List {
           </div>
           <div class="card-footer">
             <form onsubmit="app.itemsController.createItem('${this.id}')" class="d-flex gap-2 mt-2">
-              <input type="text" name="name" class="w-100 rounded" required minlength="3" maxlength="15">
+              <input type="text" name="name" class="w-100 rounded" required minlength="3" maxlength="25">
               <button class="btn btn-primary" type="submit"><i class="mdi mdi-plus"></i></button>
             </form>
           </div>
